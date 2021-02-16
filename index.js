9a74d204a218f9b1e1039cd6c3ed2f21e994f8d9
Soldy <4786022+Soldy@users.noreply.github.com>
2021-02-16 00:07:57 +0000

    shine
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- index.js
+++ index.js
@@ -9,19 +9,19 @@
 const graphrcBase=function(){
     this.render = function (data, number){
         return render(data, number);
-    }
+    };
     let bar = [
-        " ",
-        "\u2581",
-        "\u2582",
-        "\u2583",
-        "\u2584",
-        "\u2585",
-        "\u2586",
-        "\u2587",
-        "\u2588",
-     ];
-     const start  = function(length, number){
+        ' ',
+        '\u2581',
+        '\u2582',
+        '\u2583',
+        '\u2584',
+        '\u2585',
+        '\u2586',
+        '\u2587',
+        '\u2588',
+    ];
+    const start  = function(length, number){
         if (
             (typeof number !== 'number')||
             (60 > length)||
@@ -31,67 +31,67 @@ const graphrcBase=function(){
         if (60 > (length - number))
             return (length - 60);
         return number;
-     }
-     const end = function(length, first){
+    };
+    const end = function(length, first){
         if (60 > length - first) 
             return length;
         return first+60;
-     }
-     const minCalc = function(data){
+    };
+    const minCalc = function(data){
         let min = data[0];
         for (let i of data)
             if (min > parseFloat(i))
                 min = Math.floor(i);
         return min;
-     }
-     const maxCalc = function(data){
+    };
+    const maxCalc = function(data){
         let max = data[0];
         for (let i of data)
             if (parseFloat(i) > max)
                 max = Math.ceil(i);
         return max;
-     }
-     const diffCalc = function(max, min){
-         return max-min;
-     }
-     const divCalc = function(diff){
-         return diff / 100;
-     }
-     const poolCalc = function(data, div, min){
-         let pool = [];
-         for (let i of data)
-              pool.push(
-                  parseInt(
-                      (i - min) / div
-                  )
-              );
-         return pool;
-     }
-     const firstLine = function(){
-         return (' \u25B2').padEnd(60, ' ');
-     }
-     const line = function(data, line_num){
+    };
+    const diffCalc = function(max, min){
+        return max-min;
+    };
+    const divCalc = function(diff){
+        return diff / 100;
+    };
+    const poolCalc = function(data, div, min){
+        let pool = [];
+        for (let i of data)
+            pool.push(
+                parseInt(
+                    (i - min) / div
+                )
+            );
+        return pool;
+    };
+    const firstLine = function(){
+        return (' \u25B2').padEnd(60, ' ');
+    };
+    const line = function(data, line_num){
         const modify = ((14-line_num)*8);
-        let out = " \u2502";
+        let out = ' \u2502';
         for (let i of data ) {
-             let minus = parseInt(i - modify);
-             if (1 > minus)
-                 minus = 0;
-             if (minus > 7)
-                 minus = 8;
-             out+= bar[minus];
+            let minus = parseInt(i - modify);
+            if (1 > minus)
+                minus = 0;
+            if (minus > 7)
+                minus = 8;
+            out+= bar[minus];
         }
         return out.padEnd(62, ' ');
 
-     }
-     const lastLine = function(){
-         return (
-             (' \u2514').padEnd(60, '\u2500')+
+    };
+    const lastLine = function(){
+        return (
+            (' \u2514').padEnd(60, '\u2500')+
              '\u25B6 '
-         );
-     }
+        );
+    };
  
-     const render = function (data, number){
+    const render = function (data, number){
         let graph =[];
         const length = data.length-1;
         const first = start(length, number);
@@ -107,13 +107,13 @@ const graphrcBase=function(){
         );
         for (let i = 0; 14> i ; i++)
             graph.push(
-                 line(pool, i)
+                line(pool, i)
             );
         graph.push(
             lastLine()
         );
         return graph;
-     }
-}
+    };
+};
 
 exports.base = graphrcBase;
